3e3a0d3d17a2cbcea264696ca261a7588feecd5b
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-01-19 17:07:02 +0000

    Prison

--- server-data/resources/[maps]/[PRISON]/bobo_prison/__resource.lua
+++ server-data/resources/[maps]/[PRISON]/bobo_prison/__resource.lua
@@ -1,11 +1,12 @@
--- This prison has been made by Bobo Boss. My discord: https://discord.gg/jH2aZqw
--- If the prison does not load, contact Bobo Boss on discord!
-
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 this_is_a_map 'yes'
 
 fx_version 'adamant'
 game 'gta5'
+
+client_script {
+	'client.lua',
+}
 
 files {
     "cellgate_game.dat151.rel"
