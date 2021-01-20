40be0594e7d1bcf3a2ad569a124930240ed74e22
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-01-20 06:30:39 +0000

    Added server name to pause menu

--- server-data/resources/[rl-resources]/rl-scripts/client/loops.lua
+++ server-data/resources/[rl-resources]/rl-scripts/client/loops.lua
@@ -200,3 +200,11 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+  AddTextEntry('FE_THDR_GTAO', 'TwitchCityRP')
+end)
