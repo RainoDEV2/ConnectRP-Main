8754d1f11d7932e46a7a122ffe0da2b0815febe9
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-02-13 12:51:14 +0000

    Fuel usage update

--- server-data/resources/[standalone]/LegacyFuel/config.lua
+++ server-data/resources/[standalone]/LegacyFuel/config.lua
@@ -62,28 +62,28 @@ Config.RemoveHUDForBlacklistedVehicle = true
 
 -- Class multipliers. If you want SUVs to use less fuel, you can change it to anything under 1.0, and vise versa.
 Config.Classes = {
-	[0] = 1.0, -- Compacts
-	[1] = 1.0, -- Sedans
-	[2] = 1.0, -- SUVs
-	[3] = 1.0, -- Coupes
-	[4] = 1.0, -- Muscle
-	[5] = 1.0, -- Sports Classics
-	[6] = 1.0, -- Sports
-	[7] = 1.0, -- Super
-	[8] = 1.0, -- Motorcycles
-	[9] = 1.0, -- Off-road
-	[10] = 1.0, -- Industrial
-	[11] = 1.0, -- Utility
-	[12] = 1.0, -- Vans
+	[0] = 0.8, -- Compacts
+	[1] = 0.8, -- Sedans
+	[2] = 0.8, -- SUVs
+	[3] = 0.8, -- Coupes
+	[4] = 0.8, -- Muscle
+	[5] = 0.8, -- Sports Classics
+	[6] = 0.8, -- Sports
+	[7] = 0.8, -- Super
+	[8] = 0.8, -- Motorcycles
+	[9] = 0.8, -- Off-road
+	[10] = 0.8, -- Industrial
+	[11] = 0.8, -- Utility
+	[12] = 0.8, -- Vans
 	[13] = 0.0, -- Cycles
-	[14] = 1.0, -- Boats
-	[15] = 1.0, -- Helicopters
-	[16] = 1.0, -- Planes
-	[17] = 1.0, -- Service
-	[18] = 1.0, -- Emergency
-	[19] = 1.0, -- Military
-	[20] = 1.0, -- Commercial
-	[21] = 1.0, -- Trains
+	[14] = 0.8, -- Boats
+	[15] = 0.8, -- Helicopters
+	[16] = 0.8, -- Planes
+	[17] = 0.8, -- Service
+	[18] = 0.8, -- Emergency
+	[19] = 0.8, -- Military
+	[20] = 0.8, -- Commercial
+	[21] = 0.8, -- Trains
 }
 
 -- The left part is at percentage RPM, and the right is how much fuel (divided by 10) you want to remove from the tank every second
