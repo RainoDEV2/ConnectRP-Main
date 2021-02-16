fa08f5d3e5c27d552755f921e1c86ba4cc763624
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-02-16 05:35:26 +0000

    BAT updates

--- server-data/Frank.bat
+++ server-data/Frank.bat
@@ -1,1 +1,1 @@
-C:\Users\Frank\Desktop\TwitchCityRP-V1\FXServer.exe +set sv_enforceGameBuild 2060 +exec server.cfg +set onesync_enabled 1 +set onesync_population true +set onesync_enableBeyond true
+C:\Users\Frank\Desktop\TwitchCityRP-V1\FXServer.exe +exec server.cfg +set onesync_enabled 1 +set onesync_population true +set onesync_enableBeyond true
