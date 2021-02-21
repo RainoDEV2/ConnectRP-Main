e2f9d5f0d83d47261c0fc3077de57be967a9c9c9
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-02-21 21:59:37 +0000

    Update loops.lua

--- server-data/resources/[rl-resources]/rl-scripts/client/loops.lua
+++ server-data/resources/[rl-resources]/rl-scripts/client/loops.lua
@@ -177,21 +177,6 @@ Citizen.CreateThread(function()
 	end
 end)
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        local veh = GetVehiclePedIsIn(PlayerPedId(), false)
-        if DoesEntityExist(veh) and not IsEntityDead(veh) then
-            local model = GetEntityModel(veh)
-            -- If it's not a boat, plane or helicopter, and the vehilce is off the ground with ALL wheels, then block steering/leaning left/right/up/down.
-            if not IsThisModelABoat(model) and not IsThisModelAHeli(model) and not IsThisModelAPlane(model) and IsEntityInAir(veh) or IsEntityUpsidedown(veh) then
-                DisableControlAction(0, 59) -- leaning left/right
-                DisableControlAction(0, 60) -- leaning up/down
-            end
-        end
-    end
-end)
-
 function AddTextEntry(key, value)
 	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
 end
