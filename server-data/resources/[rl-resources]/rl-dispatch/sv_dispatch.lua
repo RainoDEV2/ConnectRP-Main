81fd8b07bca0c7b72063ef043867bfdb23710f19
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-02-22 21:37:55 +0000

    allowed reporters to see more

--- server-data/resources/[rl-resources]/rl-dispatch/sv_dispatch.lua
+++ server-data/resources/[rl-resources]/rl-dispatch/sv_dispatch.lua
@@ -7,12 +7,12 @@ RLCore.Commands.Add("dispatch", "Emergency Dispatch Control.", {{name="type", he
 end)
 
 local DispatchMessages = {
-    ['10-50'] = { ['label'] = 'Car crash reported', ['reporter'] = false, ['police'] = true, ['ambulance'] = false, ['priority'] = 3, ['blipSprite'] = 380, ['blipColor'] = 4 }, -- Officer Car Crash
+    ['10-50'] = { ['label'] = 'Car crash reported', ['reporter'] = true, ['police'] = true, ['ambulance'] = false, ['priority'] = 3, ['blipSprite'] = 380, ['blipColor'] = 4 }, -- Officer Car Crash
     ['10-10'] = { ['label'] = 'Fight in progress', ['reporter'] = false, ['police'] = false, ['ambulance'] = false, ['blipSprite'] = 311, ['blipColor'] = 4 }, -- Fight
-    ['10-11'] = { ['label'] = 'Deadly armed fight in a progress', ['reporter'] = false, ['police'] = true, ['ambulance'] = false, ['blipSprite'] = 313, ['blipColor'] = 4 }, -- Armed Fight
-    ['10-31A'] = { ['label'] = 'House Robbery in action', ['reporter'] = false, ['police'] = true, ['ambulance'] = false, ['blipSprite'] = 418, ['blipColor'] = 4 }, -- House Robbery
+    ['10-11'] = { ['label'] = 'Deadly armed fight in a progress', ['reporter'] = true, ['police'] = true, ['ambulance'] = false, ['blipSprite'] = 313, ['blipColor'] = 4 }, -- Armed Fight
+    ['10-31A'] = { ['label'] = 'House Robbery in action', ['reporter'] = true, ['police'] = true, ['ambulance'] = false, ['blipSprite'] = 418, ['blipColor'] = 4 }, -- House Robbery
     ['10-40A'] = { ['label'] = 'Store Robbery in action', ['reporter'] = false, ['police'] = true, ['ambulance'] = false, ['blipSprite'] = 156, ['blipColor'] = 4 }, -- Store Robbery
-    ['10-90'] = { ['label'] = 'Robbery of a bank truck in progress', ['reporter'] = false, ['police'] = true, ['ambulance'] = false, ['important'] = true, ['blipSprite'] = 67, ['blipColor'] = 2 }, -- Bank Truck Robbery
+    ['10-90'] = { ['label'] = 'Robbery of a bank truck in progress', ['reporter'] = true, ['police'] = true, ['ambulance'] = false, ['important'] = true, ['blipSprite'] = 67, ['blipColor'] = 2 }, -- Bank Truck Robbery
     ['10-32B'] = { ['label'] = 'Robbery at the Jewelry Store', ['reporter'] = true, ['police'] = true, ['ambulance'] = false, ['important'] = true, ['blipSprite'] = 439, ['blipColor'] = 3 }, -- Jewelry Robbery
 	['10-32A'] = { ['label'] = 'Robbery at the Fleeca Bank', ['reporter'] = true, ['police'] = true, ['ambulance'] = false, ['important'] = true, ['blipSprite'] = 88, ['blipColor'] = 2 }, -- Fleeca Bank Robbery
     ['10-33A'] = { ['label'] = 'Robbery at the Paleto Bank', ['reporter'] = true, ['police'] = true, ['ambulance'] = false, ['important'] = true, ['blipSprite'] = 267, ['blipColor'] = 2 }, -- Paleto Bank Robbery
@@ -25,7 +25,7 @@ local DispatchMessages = {
     ['10-71A'] = { ['label'] = 'Gun shots reported', ['reporter'] = false, ['police'] = true, ['ambulance'] = false, ['blipSprite'] = 119, ['blipColor'] = 1 }, -- Gun Shots
     ['10-71B'] = { ['label'] = 'Gun shots reported from a vehicle',['reporter'] = false, ['police'] = true, ['ambulance'] = false, ['blipSprite'] = 229, ['blipColor'] = 1 }, -- Gun Shots From Vehicle
     ['10-60'] = { ['label'] = 'Carjacking in progress',['reporter'] = false, ['police'] = true, ['ambulance'] = false, ['blipSprite'] = 225, ['blipColor'] = 4 }, -- Lockpick
-    ['10-47'] = { ['label'] = 'Injured person',['reporter'] = false, ['police'] = true, ['ambulance'] = true, ['blipSprite'] = 303, ['blipColor'] = 4 }, -- Death
+    ['10-47'] = { ['label'] = 'Injured person',['reporter'] = true, ['police'] = true, ['ambulance'] = true, ['blipSprite'] = 303, ['blipColor'] = 4 }, -- Death
     ['10-13A'] = { ['label'] = 'Officer down URGENT',['reporter'] = false, ['police'] = true, ['ambulance'] = true, ['important'] = true, ['priority'] = 3, ['sound'] = '10-1314', ['blipSprite'] = 621, ['blipColor'] = 38 }, -- Officer Down
     ['10-13B'] = { ['label'] = 'Officer down NOT URGENT',['reporter'] = false, ['police'] = true, ['ambulance'] = true, ['important'] = false, ['priority'] = 3, ['sound'] = '10-1314', ['blipSprite'] = 621, ['blipColor'] = 38 }, -- Officer Down
     ['10-14A'] = { ['label'] = 'EMS down URGENT',['reporter'] = false, ['police'] = true, ['ambulance'] = true, ['important'] = true, ['priority'] = 3, ['sound'] = '10-1314', ['blipSprite'] = 621, ['blipColor'] = 1 }, -- EMS Down
