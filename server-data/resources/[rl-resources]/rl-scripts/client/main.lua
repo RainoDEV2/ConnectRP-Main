418213883f6353f0b74224874aa01458635087bc
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-02-23 18:39:02 +0000

    Calmer water

--- server-data/resources/[rl-resources]/rl-scripts/client/main.lua
+++ server-data/resources/[rl-resources]/rl-scripts/client/main.lua
@@ -168,7 +168,7 @@ end, false) ]]
 Citizen.CreateThread(function()
 	while true do
 		Wait(0)
-		Citizen.InvokeNative(0xC54A08C85AE4D410, 0.6)
+		Citizen.InvokeNative(0xC54A08C85AE4D410, 0.3)
 	end
 end)
 
