411b1343ec7db73367d8a4dfa0d85aa72fa779c8
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-02-24 04:39:06 +0000

    test 2

--- server-data/resources/[testing]/Sil-2Step/__resource.lua
+++ server-data/resources/[testing]/Sil-2Step/__resource.lua
@@ -1,8 +1,24 @@
-resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page('html/index.html')
 
 client_scripts {
     'cl-2Step.lua',
     'Config.lua'
 }
 
-server_script 'sv-2Step.lua'
+server_scripts{
+    'sv-2Step.lua'
+}
+
+files {
+    'html/index.html',
+    'html/sounds/1.ogg',
+    'html/sounds/2.ogg',
+    'html/sounds/3.ogg',
+    'html/sounds/4.ogg',
+    'html/sounds/5.ogg',
+    'html/sounds/6.ogg'
+}
+
+
