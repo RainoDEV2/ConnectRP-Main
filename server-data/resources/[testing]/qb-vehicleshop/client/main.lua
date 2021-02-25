9e2371cd605e263d9b59242f6a23a0d396f1f727
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-02-25 19:03:54 +0000

    :eyes:

---
+++ server-data/resources/[testing]/qb-vehicleshop/client/main.lua
@@ -0,0 +1,174 @@
+Keys = {
+    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+}
+
+RLCore = nil
+isLoggedIn = false
+
+Citizen.CreateThread(function() 
+    while true do
+        Citizen.Wait(10)
+        if RLCore == nil then
+            TriggerEvent("RLCore:GetObject", function(obj) RLCore = obj end)    
+            Citizen.Wait(200)
+        end
+    end
+end)
+PlayerJob = {}
+
+--- CODE
+
+local inVehicleShop = false
+
+vehicleCategorys = {
+    ["coupes"] = {
+        label = "Coupes",
+        vehicles = {}
+    },
+    ["sedans"] = {
+        label = "Sedans",
+        vehicles = {}
+    },
+    ["muscle"] = {
+        label = "Muscle",
+        vehicles = {}
+    },
+    ["suvs"] = {
+        label = "SUVs",
+        vehicles = {}
+    },
+    ["compacts"] = {
+        label = "Compacts",
+        vehicles = {}
+    },
+    ["vans"] = {
+        label = "Vans",
+        vehicles = {}
+    },
+    ["super"] = {
+        label = "Super",
+        vehicles = {}
+    },
+    ["sports"] = {
+        label = "Sports",
+        vehicles = {}
+    },
+    ["sportsclassics"] = {
+        label = "Sports Classics",
+        vehicles = {}
+    },
+    ["motorcycles"] = {
+        label = "Motorcycles",
+        vehicles = {}
+    },
+    ["offroad"] = {
+        label = "Offroad",
+        vehicles = {}
+    },
+    ["tuner"] = {
+        label = "Tuner",
+        vehicles = {}
+    },
+}
+
+RegisterNetEvent('RLCore:Client:OnPlayerLoaded')
+AddEventHandler('RLCore:Client:OnPlayerLoaded', function()
+    RLCore.Functions.GetPlayerData(function(PlayerData)
+        PlayerJob = PlayerData.job
+    end)
+    isLoggedIn = true
+end)
+
+RegisterNetEvent('RLCore:Client:OnJobUpdate')
+AddEventHandler('RLCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerJob = JobInfo
+end)
+
+Citizen.CreateThread(function()
+    Citizen.Wait(1000)
+    for k, v in pairs(RLCore.Shared.Vehicles) do
+        if v["shop"] == "pdm" then
+            for cat,_ in pairs(vehicleCategorys) do
+                if RLCore.Shared.Vehicles[k]["category"] == cat then
+                    table.insert(vehicleCategorys[cat].vehicles, RLCore.Shared.Vehicles[k])
+                end
+            end
+        end
+    end
+end)
+
+function openVehicleShop(bool)
+    SetNuiFocus(bool, bool)
+    SendNUIMessage({
+        action = "ui",
+        ui = bool
+    })
+end
+
+function setupVehicles(vehs)
+    SendNUIMessage({
+        action = "setupVehicles",
+        vehicles = vehs
+    })
+end
+
+RegisterNUICallback('GetCategoryVehicles', function(data)
+    setupVehicles(shopVehicles[data.selectedCategory])
+end)
+
+RegisterNUICallback('exit', function()
+    openVehicleShop(false)
+end)
+
+RegisterNUICallback('buyVehicle', function(data)
+    local vehicleData = data.vehicleData
+    local garage = data.garage
+
+    TriggerServerEvent('tc-vehicleshop:server:buyVehicle', vehicleData, garage)
+    openVehicleShop(false)
+end)
+
+RegisterNetEvent('tc-vehicleshop:client:spawnBoughtVehicle')
+AddEventHandler('tc-vehicleshop:client:spawnBoughtVehicle', function(vehicle)
+    RLCore.Functions.SpawnVehicle(vehicle, function(veh)
+        SetEntityHeading(veh, RL.SpawnPoint.h)
+        TaskWarpPedIntoVehicle(GetPlayerPed(-1), veh, -1)
+    end, RL.SpawnPoint, true)
+end)
+
+Citizen.CreateThread(function()
+    for k, v in pairs(RL.VehicleShops) do
+        Dealer = AddBlipForCoord(RL.VehicleShops[k].x, RL.VehicleShops[k].y, RL.VehicleShops[k].z)
+
+        SetBlipSprite (Dealer, 326)
+        SetBlipDisplay(Dealer, 4)
+        SetBlipScale  (Dealer, 0.75)
+        SetBlipAsShortRange(Dealer, true)
+        SetBlipColour(Dealer, 37)
+
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentSubstringPlayerName("Premium Deluxe Motorsports")
+        EndTextCommandSetBlipName(Dealer)
+    end
+end)
+
+Citizen.CreateThread(function()
+    QuickSell = AddBlipForCoord(RL.QuickSell.x, RL.QuickSell.y, RL.QuickSell.z)
+
+    SetBlipSprite (QuickSell, 605)
+    SetBlipDisplay(QuickSell, 4)
+    SetBlipScale  (QuickSell, 0.55)
+    SetBlipAsShortRange(QuickSell, true)
+    SetBlipColour(QuickSell, 2)
+
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName("PDM Quick Car Sell")
+    EndTextCommandSetBlipName(QuickSell)
+end)
