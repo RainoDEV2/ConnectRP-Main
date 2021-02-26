07fb7c0d46c96a2d183ecf479196e2f4bf573859
Kazumi <76877520+kazumi-dev@users.noreply.github.com>
2021-02-26 01:07:33 +0000

    rem teleport

--- server-data/resources/[rl-resources]/rl-scripts/client/teleports.lua
+++ server-data/resources/[rl-resources]/rl-scripts/client/teleports.lua
@@ -77,7 +77,8 @@ Citizen.CreateThread(function()
                         if IsControlJustReleased(0, 51) then
 
                             if v.x == 935.81481 then
-                                TriggerEvent("casino:client:enter")
+                                --TriggerEvent("casino:client:enter")
+                                RLCore.Functions.Notify('The casino is closed due to renovation')
                             else
                                 if k == 1 then
                                     SetEntityCoords(ped, Teleports.Locations[loc][2].x, Teleports.Locations[loc][2].y, Teleports.Locations[loc][2].z)
