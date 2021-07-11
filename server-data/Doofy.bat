f3aa4d5fbe78f2ac2aba1766ff91557ab4d87e40
Rhodinium <kazumidevelopment@gmail.com>
2021-07-12 00:07:12 +0100

    ReStart?

--- server-data/Doofy.bat
+++ server-data/Doofy.bat
@@ -1,1 +1,1 @@
-C:\[SERVERS]\TwitchCityRP-V1\FXServer.exe +exec server.cfg +set onesync_population true +set onesync_enableBeyond true +set sv_enforceGameBuild 2189
+D:\FiveM-Servers\TwitchCityRP\FXServer.exe +exec server.cfg +set onesync_population true +set onesync_enableBeyond true +set sv_enforceGameBuild 2189
