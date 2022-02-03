48508066c0250e781722e3f6040b184468e3043b
Rhodinium <curtislee1994@live.co.uk>
2022-02-03 23:49:49 +0000

    Some update thing

--- server-data/Doofy.bat
+++ server-data/Doofy.bat
@@ -1,1 +1,2 @@
-D:\FiveM-Servers\TwitchCityRP\FXServer.exe +exec server.cfg +set onesync_population true +set onesync_enableBeyond true +set sv_enforceGameBuild 2189
+C:\TwitchCityRP-V1\FXServer.exe +exec server.cfg +set onesync_population true +set onesync_enableBeyond true +set sv_enforceGameBuild 2189
+
