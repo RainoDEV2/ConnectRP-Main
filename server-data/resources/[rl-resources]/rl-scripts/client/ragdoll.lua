b62d08f9074ef14cb50db85bfc999cbf2a22203b
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-03-03 07:37:19 +0000

    Update ragdoll.lua

--- server-data/resources/[rl-resources]/rl-scripts/client/ragdoll.lua
+++ server-data/resources/[rl-resources]/rl-scripts/client/ragdoll.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1)
-		SetPedMinGroundTimeForStungun(GetPlayerPed(-1), 7000)
+		SetPedMinGroundTimeForStungun(GetPlayerPed(-1), 3500)
 	end
 end)
 
