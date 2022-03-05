eda9c44753e561dbe979a9148f643f2a4f4b8da6
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-03-05 12:15:25 +0000

    Sound API update (global 10-13?)

--- server-data/resources/[standalone]/interact-sound/client/main.lua
+++ server-data/resources/[standalone]/interact-sound/client/main.lua
@@ -8,8 +8,12 @@
 -- a specific range from the entity to which the sound has been created.
 ------
 
-local standardVolumeOutput = 1.0;
-
+local standardVolumeOutput = 0.3;
+local hasPlayerLoaded = false
+Citizen.CreateThread(function()
+	Wait(15000)
+	hasPlayerLoaded = true
+end)
 ------
 -- RegisterNetEvent LIFE_CL:Sound:PlayOnOne
 --
@@ -23,12 +27,15 @@ local standardVolumeOutput = 1.0;
 ------
 RegisterNetEvent('InteractSound_CL:PlayOnOne')
 AddEventHandler('InteractSound_CL:PlayOnOne', function(soundFile, soundVolume)
-    SendNUIMessage({
-        transactionType     = 'playSound',
-        transactionFile     = soundFile,
-        transactionVolume   = soundVolume
-    })
+    if hasPlayerLoaded then
+        SendNUIMessage({
+            transactionType = 'playSound',
+            transactionFile  = soundFile,
+            transactionVolume = soundVolume
+        })
+    end
 end)
+
 ------
 -- RegisterNetEvent LIFE_CL:Sound:PlayOnAll
 --
@@ -42,11 +49,13 @@ end)
 ------
 RegisterNetEvent('InteractSound_CL:PlayOnAll')
 AddEventHandler('InteractSound_CL:PlayOnAll', function(soundFile, soundVolume)
-    SendNUIMessage({
-        transactionType     = 'playSound',
-        transactionFile     = soundFile,
-        transactionVolume   = soundVolume
-    })
+    if hasPlayerLoaded then
+        SendNUIMessage({
+            transactionType = 'playSound',
+            transactionFile = soundFile,
+            transactionVolume = soundVolume or standardVolumeOutput
+        })
+    end
 end)
 
 ------
@@ -66,18 +75,18 @@ end)
 -- @TODO Change sound volume based on the distance the player is away from the playOnEntity.
 ------
 RegisterNetEvent('InteractSound_CL:PlayWithinDistance')
-AddEventHandler('InteractSound_CL:PlayWithinDistance', function(playerNetId, maxDistance, soundFile, soundVolume)
-    local target = GetPlayerFromServerId(playerNetId)
-    if target ~= nil and target ~= -1 then
-        local lCoords = GetEntityCoords(GetPlayerPed(-1))
-        local eCoords = GetEntityCoords(GetPlayerPed(target))
-        local distIs  = Vdist(lCoords.x, lCoords.y, lCoords.z, eCoords.x, eCoords.y, eCoords.z)
-        if(distIs <= maxDistance) then
-            SendNUIMessage({
-                transactionType     = 'playSound',
-                transactionFile     = soundFile,
-                transactionVolume   = soundVolume
-            })
-        end
-    end
+AddEventHandler('InteractSound_CL:PlayWithinDistance', function(otherPlayerCoords, maxDistance, soundFile, soundVolume)
+	if hasPlayerLoaded then
+		local myCoords = GetEntityCoords(PlayerPedId())
+		local distance = #(myCoords - otherPlayerCoords)
+
+		if distance < maxDistance then
+			SendNUIMessage({
+				transactionType = 'playSound',
+				transactionFile  = soundFile,
+				transactionVolume = soundVolume or standardVolumeOutput
+			})
+		end
+	end
 end)
+
