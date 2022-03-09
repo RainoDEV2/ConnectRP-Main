815080f3083b87de9db78c7266ef19f88700cae1
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-03-09 21:38:28 +0000

    Stuff and things

--- server-data/Doofy.bat
+++ server-data/Doofy.bat
@@ -1,2 +1,2 @@
-C:\TwitchCityRP-V1\FXServer.exe +exec server.cfg +set onesync_population true +set onesync_enableBeyond true 
+D:\[servers]\ConnectRP-Main\FXServer.exe +exec server.cfg +set onesync_population true +set onesync_enableBeyond true 
 
