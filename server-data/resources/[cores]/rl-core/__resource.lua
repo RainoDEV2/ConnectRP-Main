4ee53ad8ca84b69e11e80e8f83453b38633ffb3e
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-03-12 11:16:55 +0000

    Locale changes

--- server-data/resources/[cores]/rl-core/__resource.lua
+++ server-data/resources/[cores]/rl-core/__resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 shared_script "@nevo-scripts/cl_errorlog.lua"
 shared_script "locale/en.lua"
-shared_script "locale.lua"
+shared_script "shared/locale.lua"
 
 server_scripts {
 	"config.lua",
