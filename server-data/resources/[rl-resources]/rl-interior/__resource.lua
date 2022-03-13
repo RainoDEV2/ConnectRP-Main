0a18992cb9ee07888f713cb1f9457884ac467814
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-03-13 22:06:10 +0000

    Apartment interior updated

--- server-data/resources/[rl-resources]/rl-interior/__resource.lua
+++ server-data/resources/[rl-resources]/rl-interior/__resource.lua
@@ -21,6 +21,7 @@ files {
 	'stream/frankelientje/frankelientje.ytyp',
 	'stream/tante_shell/tante.ytyp',
 	'stream/methlab_shell/methlab_shell.ytyp',
+	'stream/pinkcage/gabz_pinkcage.ytyp'
 }
 
 data_file 'DLC_ITYP_REQUEST' 'stream/v_int_20.ytyp'
@@ -38,6 +39,7 @@ data_file 'DLC_ITYP_REQUEST' 'stream/caravan_shell/caravan.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/frankelientje/frankelientje.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/tante_shell/tante.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/methlab_shell/methlab_shell.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pinkcage/gabz_pinkcage.ytyp'
 
 exports {
 	'DespawnInterior',
