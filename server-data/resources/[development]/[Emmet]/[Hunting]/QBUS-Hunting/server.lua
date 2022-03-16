f48330b3e3eb4ca10e82bd9cf56930871cb33eaa
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-03-16 21:21:56 +0000

    Update server.lua

--- server-data/resources/[development]/[Emmet]/[Hunting]/QBUS-Hunting/server.lua
+++ server-data/resources/[development]/[Emmet]/[Hunting]/QBUS-Hunting/server.lua
@@ -82,27 +82,27 @@ RegisterNetEvent('Dox-hunting:server:sell')
 	if args == 1 then 
 		if bankBalance >= rifleprice then
 			Player.Functions.RemoveMoney('bank', rifleprice, "CHANGEME")
-			Player.Functions.AddItem('fishbait', 1, nil, {["quality"] = 100})
+			Player.Functions.AddItem('CHANGEME', 1, nil, {["quality"] = 100})
 			TriggerClientEvent('inventory:client:ItemBox', src, RLCore.Shared.Items['CHANGEME'], "add", 1)
-			TriggerClientEvent("doj:client:buyFishingGear", source)
+			--TriggerClientEvent("doj:client:buyFishingGear", source)
 		else
 			TriggerClientEvent('RLCore:Notify', src, "You dont have enough money..", "error")
 		end 
 	elseif args == 2 then 
 		if bankBalance >= baitprice then
 			Player.Functions.RemoveMoney('bank', baitprice, "huntingbait")
-			Player.Functions.AddItem('fishingrod', 1, nil, {["quality"] = 100})
+			Player.Functions.AddItem('huntingbait', 1, nil, {["quality"] = 100})
 			TriggerClientEvent('inventory:client:ItemBox', src, RLCore.Shared.Items['huntingbait'], "add", 1)
-			TriggerClientEvent("doj:client:buyFishingGear", source)
+			--TriggerClientEvent("doj:client:buyFishingGear", source)
 		else
 			TriggerClientEvent('RLCore:Notify', src, "You dont have enough money..", "error")
 		end
 	elseif args == 3 then 
 		if bankBalance >= knifeprice then
 			Player.Functions.RemoveMoney('bank', knifeprice, "WEAPON_KNIFE")
-			Player.Functions.AddItem('anchor', 1, nil, {["quality"] = 100})
-			TriggerClientEvent('inventory:client:ItemBox', src, RLCore.Shared.Items['WEAPON_KNIFE'], "add", 1)
-			TriggerClientEvent("doj:client:buyFishingGear", source)
+			Player.Functions.AddItem('WEAPON_KNIFE', 1, nil, {["quality"] = 100})
+			TriggerClientEvent('inventory:client:ItemBox', src, RLCore.Shared.Items['WEAPON_KNIFE'], "add", 1) 
+			--TriggerClientEvent("doj:client:buyFishingGear", source)
 		else
 			TriggerClientEvent('RLCore:Notify', src, "You dont have enough money..", "error")
 		end
