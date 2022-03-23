88cc82c012fc3a44cf301acf06b7dc0768fbdd79
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-03-23 15:55:48 +0000

    New Doorlock System

--- server-data/resources/[standalone]/nui_doorlock/config.lua
+++ server-data/resources/[standalone]/nui_doorlock/config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.ShowUnlockedText = true
 
 Config.LockedColor = 'rgb(219 58 58)'
-Config.UnlockedColor = 'rgb(19, 28, 74)' -- Old Color if you want it 'rgb(19, 28, 74)'
+Config.UnlockedColor = 'rgb(33, 149, 246)' -- Old Color if you want it 'rgb(19, 28, 74)'
 
 Config.AdminAccess = {
 	enabled = false,
@@ -12,7 +12,7 @@ Config.AdminAccess = {
 Config.CommandPermission = 'god' -- Needs to be admin or god, if you want no permission on it you'd have to remove some code
 
 Config.Debug = false -- Prints the closest door data
-
+ 
 Config.DoorList = {
 
 }
@@ -20,7 +20,7 @@ Config.DoorList = {
 -- PD Main Doors created by Doofy Gilmore
 Config.DoorList['PD Main Doors'] = {
     audioRemote = false,
-    locked = true,
+    locked = false,
     doors = {
         {objHash = -1547307588, objHeading = 269.98272705078, objCoords = vec3(434.744446, -980.755554, 30.815304)},
         {objHash = -1547307588, objHeading = 90.017288208008, objCoords = vec3(434.744446, -983.078125, 30.815304)}
